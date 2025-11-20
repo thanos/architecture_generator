c95b3dd46a4ea631053e3da96e17c36a2d6de348
thanos <thanosv@gmail.com>
2025-11-20 04:14:39 +0000

    configure Bandit adapter in dev.exs

--- config/dev.exs
+++ config/dev.exs
@@ -20,6 +20,7 @@ config :architecture_generator, ArchitectureGeneratorWeb.Endpoint,
   # Binding to loopback ipv4 address prevents access from other machines.
   # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
   http: [ip: {127, 0, 0, 1}, port: String.to_integer(System.get_env("PORT") || "4000")],
+  adapter: Bandit.PhoenixAdapter,
   check_origin: false,
   code_reloader: true,
   debug_errors: true,
