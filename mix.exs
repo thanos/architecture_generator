015ee825adb5667f54640f1c8f1cd2cabe395c37
thanos <thanosv@gmail.com>
2025-11-20 04:06:51 +0000

    switch to PostgreSQL and Oban

--- mix.exs
+++ mix.exs
@@ -37,7 +37,7 @@ defmodule ArchitectureGenerator.MixProject do
       {:phoenix, "~> 1.8.0-rc.4", override: true},
       {:phoenix_ecto, "~> 4.5"},
       {:ecto_sql, "~> 3.13"},
-      {:ecto_sqlite3, ">= 0.0.0"},
+      {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 4.1"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_view, "~> 1.1.0-rc.0"},
@@ -59,7 +59,7 @@ defmodule ArchitectureGenerator.MixProject do
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.2.0"},
       {:bandit, "~> 1.5"},
-      {:que, "~> 0.10.1"}
+      {:oban, "~> 2.18"}
     ]
   end
 
