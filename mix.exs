41716c5c02c43cb60fd1e5330b1115b98b9dbef0
thanos <thanosv@gmail.com>
2025-11-20 03:49:25 +0000

    Fix mix.exs syntax error for oban dependency

--- mix.exs
+++ mix.exs
@@ -58,7 +58,7 @@ defmodule ArchitectureGenerator.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.2.0"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"}, # Added comma here
       {:oban, "~> 2.18"}
     ]
   end
