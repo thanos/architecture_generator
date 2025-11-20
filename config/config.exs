ffb60c3b17936e64d34281a4a01999d22e0f6b6a
thanos <thanosv@gmail.com>
2025-11-20 03:56:59 +0000

    fix logger format in config.exs

--- config/config.exs
+++ config/config.exs
@@ -61,13 +61,11 @@ config :tailwind,
   ]
 
 # Configures Elixir's Logger
-config :logger, :console, format: "$time $metadata[$level] $message\n_id]
+config :logger, :console, format: "$time $metadata[$level] $message\n"
 
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
-import_config "
-
-# {config_env()}.exs"
+import_config "#{config_env()}.exs"
